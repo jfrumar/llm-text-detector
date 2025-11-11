88db6426aebb345316e2a778059f96e544dba163
Julian Frumar <julian@frumar.com>
2025-11-11 11:14:26 -0800

    Add basic form and layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,55 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup, FieldLabel, FieldSet } from "@/components/ui/field";
+import { Textarea } from "@/components/ui/textarea";
+import { Bot, ClipboardPaste } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div className="min-w-68">
+      <header className="w-full bg-sky-100 py-2 px-4 bg-linear-to-b
+      from-slate-100 to-slate-200  border-b-slate-200 border-b-2 dark:bg-slate-900">
+        <h1 className="font-bold text-lg/5">LLM Text Detector</h1>
+        <h3 className="text-xs/5">Identify AI-generated content</h3>
+      </header>
+
+      <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+        <main className="flex min-h-screen w-full max-w-3xl flex-col justify-between  mt-10 px-16 bg-white dark:bg-black items-start">
+          <form className="w-full border rounded-lg min-w-48">
+            <div className="mb-5 border-b py-3 px-4">
+              <div className="flex gap-2 items-center">
+                <ClipboardPaste className="size-4" />
+                <h2 className="font-semibold text-lg">Paste Your Text</h2>
+              </div>
+              <p className="text-sm text-muted-foreground">
+                Enter the text you want to analyze for AI generation patterns
+              </p>
+            </div>
+
+            <FieldGroup className="p-4 py-0">
+              <FieldSet>
+                <FieldGroup>
+                  <Field>
+                    <Textarea
+                      id="checkout-7j9-optional-comments"
+                      placeholder="Paste your text here for AI detection analysis..."
+                      className="h-64"
+                    />
+                  </Field>
+                </FieldGroup>
+              </FieldSet>
+              <Field orientation="horizontal" className="pb-4">
+                <Button type="submit">
+                  <Bot />
+                  Submit
+                </Button>
+                <Button variant="outline" type="button">
+                  Cancel
+                </Button>
+              </Field>
+            </FieldGroup>
+          </form>
+        </main>
+      </div>
     </div>
   );
 }
